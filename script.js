8c3d44c4b40e312826a1cf2b7b346f147ef0daaa
xolanexxo-coder <xolanexxo@gmail.com>
2026-09-01 17:51:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1067,7 +1067,7 @@ function createHintText() {
   canvas.width = canvas.height = canvasSize;
   const context = canvas.getContext('2d');
   const fontSize = 50;
-  const text = 'Happy Girlfriend Day!';
+  const text = 'I MISS U BABEE';
   context.font = `bold ${fontSize}px Arial, sans-serif`;
   context.textAlign = 'center';
   context.textBaseline = 'middle';
